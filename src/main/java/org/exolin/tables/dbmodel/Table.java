dafc77d8d1572797121c6dc63cdd0e9b7ed6e1e7
T. G <thomas.goelles@gmx.net>
2020-01-09 12:21:01 +0100

    Create Table.java

---
+++ src/main/java/org/exolin/tables/dbmodel/Table.java
@@ -0,0 +1,28 @@
+package org.exolin.tables.dbmodel;
+
+/**
+  * A relational table
+  */
+class Table
+{
+  /**
+    * the name of the table
+    */
+  private String name;
+  
+  /**
+    * the column that is the primary key (composite primary keys aren't supported).
+    * This column isn't contained in {@link #dataColumns}
+    */
+  private final Column idColumn;
+  
+  private final List<Column> dataColumns = new ArrayList<>();
+  
+  public List<Column> getColumns()
+  {
+    List<Column> columns = new ArrayList<>(1+dataColumns.size());
+    columns.add(idColumn);
+    columns.addAll(dataColumns);
+    return columns;
+  }
+}
